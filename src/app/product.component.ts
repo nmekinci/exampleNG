54ad1b7a20399cd77b6911eb548ae30ef9073d3e
Must <nm.ekinci@gmail.com>
2024-01-05 11:29:04 +0100

    up

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -3,11 +3,11 @@ import { ProductRepository } from './repository.model';
 
 @Component({
   selector: 'app',
-  template: `
-  {{text | summary:15}}
-  `,
+  templateUrl:'product.component.html' ,
   styleUrls: ['product.component.css'],
 })
 export class ProductComponent {
- text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit.";
+ model: ProductRepository = new ProductRepository();
+
+ 
 }
