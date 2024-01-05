2d4f9b2257e87be6dea08e300ac3689f589b5913
Must <nm.ekinci@gmail.com>
2024-01-05 10:33:28 +0100

    up

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,36 +1,43 @@
-
-import { Component } from "@angular/core";
-import { ProductRepository } from "./repository.model";
-
+import { Component } from '@angular/core';
+import { ProductRepository } from './repository.model';
 
 @Component({
-    selector: 'app',
-    // template:`
-    // <input [value]="email" (keyup.enter)="onKeyUp($event)">
-    // `,
-    template:`
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()">
-    <br>
-    <span>{{email}}</span>
-    `,
-    styleUrls: ['product.component.css']
-})
+  selector: 'app',
+  template: `
+  <h4> Lower-Upper-Title CasePipe </h4> 
+  <p>{{title}}</p>
+  <p>{{title | lowercase}}</p>
+  <p>{{title | uppercase}}</p>
+  <p>{{title | titlecase}}</p>
 
+  <h4> Date Pipe </h4>
+  <p>{{today}}</p>
+  <p>{{today | date}}</p>
+  <p>{{today | date : 'short'}}</p>
+  <p>{{today | date : 'shortDate'}}</p>
+  <p>{{today | date : 'midium'}}</p>
+  <p>{{today | date : 'shortTime'}}</p>
+  <p>{{today | date : 'hh:mm:ss'}}</p>
+
+  <h4> Decimal Pipe </h4>
+  <p>{{students}}</p>
+  <p>{{students | number}}</p>
+  <p>{{price | number:'1.3-3'}}</p>
+
+  <h4> Currency Pipe </h4>
+  <p>{{price | currency}}</p>
+  <p>{{price | currency: 'EUR'}}</p>
+
+  <h4> Percent Pipe </h4>
+  <p>{{completed | percent}}</p>
+  <p>{{completed | percent: '3.2-2'}}</p>
+  `,
+  styleUrls: ['product.component.css'],
+})
 export class ProductComponent {
-    model: ProductRepository = new ProductRepository();
-    email = "email@email.com";
-    
-   
-    // onKeyUp(event: Event) {
-    //     const target = event.target as HTMLInputElement;
-    //     if (target) {
-    //         this.email = target.value;
-    //         console.log(this.email);
-    //     }
-    // }
-    onKeyUp() {
-        
-            console.log(this.email);
-        }
-    }
-    
+  today: number = Date.now();
+  title: string = 'Angular learning';
+  students: number = 21536;
+  price: number = 395.12399;
+  completed: number = 0.26;
+}
