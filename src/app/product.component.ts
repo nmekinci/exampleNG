cd474ae276a4c389a91b3b81bdc9a3d8db8f661a
Must <nm.ekinci@gmail.com>
2024-01-16 10:44:48 +0100

    ngForm class

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -4,47 +4,52 @@ import { Product } from './product.model';
 
 @Component({
   selector: 'app',
-  templateUrl:'product.component.html' ,
+  templateUrl: 'product.component.html',
   styleUrls: ['product.component.css'],
 })
 export class ProductComponent {
- model: ProductRepository = new ProductRepository();
+  model: ProductRepository = new ProductRepository();
 
- newProduct :Product = new Product()
+  newProduct: Product = new Product();
 
-//  productName : string | undefined = this.model.getProductById(1)?.name 
+  //  productName : string | undefined = this.model.getProductById(1)?.name
 
-get jsonProduct() {
-  return JSON.stringify(this.newProduct)
-}
-
-addProduct(p: Product) {
-  console.log("New Pro" + this.jsonProduct);
-}
-
-log(x:any){
-console.log(x);
-}
-
-getValidationErrors(state: any){
-let ctrlName:string = state.name
-let messages:string[] = []
-if(state.errors){
-  for ( let errorName in state.errors) {
-    switch (errorName) {
-      case "required":
-        messages.push(`You  must enter a ${ctrlName}`)
-        break;
-      case "minlength":
-        messages.push(`min 3 chars ${ctrlName}`)
-        break;
-      case "pattern":
-        messages.push(`only chars and spaces ${ctrlName}`)
-        break;
-    }
+  get jsonProduct() {
+    return JSON.stringify(this.newProduct);
   }
-}
-return messages;
-}
+
+  addProduct(p: Product) {
+    console.log('New Pro' + this.jsonProduct);
+  }
+
+  log(x: any) {
+    console.log(x);
+  }
+
+  submitForm(form:any){
+    console.log(form);
+  }
+
+  getValidationErrors(state: any) {
+    let ctrlName: string = state.name;
+    let messages: string[] = [];
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case 'required':
+            messages.push(`You  must enter a ${ctrlName}`);
+            break;
+          case 'minlength':
+            messages.push(`min 3 chars ${ctrlName}`);
+            break;
+          case 'pattern':
+            messages.push(`only chars and spaces ${ctrlName}`);
+            break;
+        }
+      }
+    }
+    return messages;
+  }
+
 
 }
