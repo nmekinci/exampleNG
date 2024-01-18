31064489dd2738740330c3d7a032c6a370543d15
Must <nm.ekinci@gmail.com>
2024-01-18 18:15:04 +0100

    post requests

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Post } from './posts.model';
 
 @Component({
   selector: 'posts',
@@ -7,12 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './posts.component.css'
 })
 export class PostsComponent {
-  posts:any;
+  posts:[any] = [{}];
+  private url:string = 'https://jsonplaceholder.typicode.com/posts'
 
   constructor(private http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+    http.get(this.url)
     .subscribe(res => {
-      this.posts = res;
+      this.posts = <[any]>res;
     } )
   }
+
+  createPost(input:HTMLInputElement) {
+    const post:Post = {title: input.value};
+    input.value = ''
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe((res: {id?:string}) => {
+      post.id = res.id;
+      this.posts.splice(0,0,post)
+      console.log(res);
+    })
+  }
+
 }
